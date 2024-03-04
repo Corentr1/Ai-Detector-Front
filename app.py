fb1b93233a207a85c6413597380e8ede3b4b1296
Alexis Draguet <58481938+alxdrgt@users.noreply.github.com>
2024-03-04 14:40:39 +0100

    basic layout + image uploader

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import os
 import streamlit as st
+import requests
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+
 
 
 # Define the base URI of the API
@@ -17,32 +22,57 @@ BASE_URI = BASE_URI if BASE_URI.endswith('/') else BASE_URI + '/'
 url = BASE_URI + 'predict'
 
 # Just displaying the source for the API. Remove this in your final version.
-st.markdown(f"Working with {url}")
-
-st.markdown("Now, the rest is up to you. Start creating your page.")
-
+#st.markdown(f"Working with {url}")
 
 # TODO: Add some titles, introduction, ...
-st.markdown("""# AI Detector""")
 
-st.markdown("""## Uncover the Origins of Your Images with Cutting-Edge AI Technology""")
+st.image('media/logo.png')
+#st.markdown("<h1 style='text-align: center;'>AI Detector</h1>", unsafe_allow_html=True)
 
-st.markdown("""Step into the world of AI Detector – your ultimate solution for /
-            unraveling the mysteries behind images. Leveraging state-of-the-art /
-            artificial intelligence, our app distinguishes between images generated /
-            by AI algorithms and those crafted by human hands. Simply upload an image, /
-            and let our intelligent algorithms dissect its nuances. Gain profound /
-            insights into the authenticity of your visuals with unmatched accuracy. /
+st.markdown("<h2 style='text-align: center;'>Uncover the Origins of Your Images with Cutting-Edge AI Technology</h2>", unsafe_allow_html=True)
+st.markdown("</p>", unsafe_allow_html=True)
+st.markdown("""Step into the world of AI Detector – your ultimate solution for \
+            unraveling the mysteries behind images. Leveraging state-of-the-art \
+            artificial intelligence, our app distinguishes between images generated \
+            by AI algorithms and those crafted by human hands. Simply upload an image, \
+            and let our intelligent algorithms dissect its nuances. Gain profound \
+            insights into the authenticity of your visuals with unmatched accuracy. \
             Embark on a journey into the future of image verification with AI Detector!""")
 
 # TODO: Request user input
+st.markdown("</p>", unsafe_allow_html=True)
+st.markdown("</p>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center;'>Upload your picture</h3>", unsafe_allow_html=True)
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
+uploaded_file = st.file_uploader("Upload an image", type=['png', 'jpg'])
+
+
+if uploaded_file is not None:
+    data = mpimg.imread(uploaded_file)
+    st.image(data)
 
 
 # TODO: Call the API using the user's input
+
+params = {
+
+}
+
+def fetch_aidetector(params):
+    """
+    Get lyrics from Seeds Lyrics API. Returns empty string if song not found
+    """
+    response = requests.get(url=url, params = params).json()
+    return round(response["fare"],2)
+
 #   - url is already defined above
 #   - create a params dict based on the user's input
 #   - finally call your API using the requests package
 
+# if st.button("Is it fake?"):
+#     response = fetch_aidetector(params)
+#     st.write(f"Your image is {response}$")
 
 # TODO: retrieve the results
 #   - add a little check if you got an ok response (status code 200) or something else
